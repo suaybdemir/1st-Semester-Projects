671c4a5605bdb61fe5601b3232b649bfe63c9187
Suayb <byausrimed@gmail.com>
2023-03-11 20:18:29 +0300

    Modified

--- Week2_1/src/week2_1/Sorts_efficients.java
+++ Week2_1/src/week2_1/Sorts_efficients.java
@@ -6,6 +6,7 @@
 package week2_1;
 import java.util.Random;
 
+
 /**
  *
  * @author PC
@@ -24,11 +25,11 @@ public class Sorts_efficients {
         for (int i = 0; i < 1000; i++) {
             arr[i]= r.nextInt();
         }
-        
+        int n = (int) (arr.length/2) + 1 ;
         selection_sort(arr);
         bubble_sort(arr);
         insertion_sort(arr);       
-        comb_sort(arr);
+        comb_sort(arr,n);
 
     }
     public static int[] insertion_sort(int arr[])
@@ -69,22 +70,31 @@ public class Sorts_efficients {
         System.out.println(" Bubble Sort su kadar surede : " + (finishTime - startTime) + " ms");
         return arr;
     }
-    public static int[] comb_sort(int arr[])
+    public static int[] comb_sort(int arr[],int n)
     {
         long startTime = System.currentTimeMillis();
-        for (int i = 0; i < (int) (arr.length/2) + 1 ; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if(arr[j]>arr[j+i])
-                {
-                    int tmp = arr[j+i];
-                    arr[j+i]=arr[j];
-                    arr[j+i]=tmp;
-                }
+        int degisim = 1 , g , i , gap=n ;
+        while ( (gap !=1 ) || (degisim==1) )
+        {
+            gap = (int) (gap/1.3) ;
+            if(gap<1) 
+            {
+                gap = 1;
+                degisim = 0;
+            }
+        }
+        for (int j = 0; n-gap > j; j++) {
+            if(arr[j]>arr[j+gap])
+            {
+                g = arr[j];
+                arr[j]=arr[j+gap];
+                arr[j+gap]=g;
+                degisim=1;
             }
         }
         long finishTime = System.currentTimeMillis();
         System.out.println(" Comb Sort su kadar surede : " + (finishTime - startTime) + " ms");
-       return arr;
+        return arr;
     }
     public static int[] selection_sort(int arr[])
     {
